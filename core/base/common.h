d1b1fcfac4fb18cb8badee47f6872b170246b3d6
haobayuxi <whaohit@gmail.com>
2024-05-23 16:53:24 +0800

    update

--- core/base/common.h
+++ core/base/common.h
@@ -16,9 +16,10 @@ using node_id_t = int;        // Machine id type
 using mr_id_t = int;          // Memory region id type
 using table_id_t = uint64_t;  // Table id type
 using itemkey_t = uint64_t;   // Data item key type, used in DB tables
-using offset_t = int64_t;     // Offset type. Usually used in remote offset for RDMA
-using version_t = uint64_t;   // Version type, used in version checking
-using lock_t = uint64_t;      // Lock type, used in remote locking
+using offset_t =
+    int64_t;  // Offset type. Usually used in remote offset for RDMA
+using version_t = uint64_t;  // Version type, used in version checking
+using lock_t = uint64_t;     // Lock type, used in remote locking
 
 // Memory region ids for server's hash store buffer and undo log buffer
 const mr_id_t SERVER_HASH_BUFF_ID = 97;
@@ -31,13 +32,14 @@ const mr_id_t CLIENT_MR_ID = 100;
 const uint64_t MEM_STORE_META_END = 0xE0FF0E0F;
 
 // Node and thread conf
-#define BACKUP_DEGREE 2          // Backup memory node number. MUST **NOT** BE SET TO 0
+#define BACKUP_DEGREE 2  // Backup memory node number. MUST **NOT** BE SET TO 0
 #define MAX_REMOTE_NODE_NUM 100  // Max remote memory node number
 #define MAX_DB_TABLE_NUM 15      // Max DB tables
 
 // Data state
 #define STATE_INVISIBLE 0x8000000000000000  // Data cannot be read
-#define STATE_LOCKED 1                      // Data cannot be written. Used for serializing transactions
+#define STATE_LOCKED \
+  1  // Data cannot be written. Used for serializing transactions
 #define STATE_CLEAN 0
 
 // Alias
@@ -48,3 +50,15 @@ const uint64_t MEM_STORE_META_END = 0xE0FF0E0F;
 // Helpful for improving condition prediction hit rate
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #define likely(x) __builtin_expect(!!(x), 1)
+
+uint64_t compute_hash(string const& s) {
+  const int p = 31;
+  const int m = 1e9 + 9;
+  uint64_t hash_value = 0;
+  uint64_t p_pow = 1;
+  for (char c : s) {
+    hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+    p_pow = (p_pow * p) % m;
+  }
+  return hash_value;
+}
--- core/metadata/dmc.h
+++ core/metadata/dmc.h
@@ -113,20 +113,8 @@ void DMC::AddToReadOnlySet(DataItemPtr item) {
   reader_set.emplace_back(data_set_item);
 }
 
-uint64_t get_time_offset(uint64_t addr) { return addr + sizeof(uint64_t); }
+// uint64_t get_time_offset(uint64_t addr) { return addr + sizeof(uint64_t); }
 
-uint64_t get_permission_offset(uint64_t addr) { return addr; }
-
-uint64_t compute_hash(string const& s) {
-  const int p = 31;
-  const int m = 1e9 + 9;
-  uint64_t hash_value = 0;
-  uint64_t p_pow = 1;
-  for (char c : s) {
-    hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
-    p_pow = (p_pow * p) % m;
-  }
-  return hash_value;
-}
+// uint64_t get_permission_offset(uint64_t addr) { return addr; }
 
 #endif
