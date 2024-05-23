e54e6dd61cddde3fc10126949a7a16c4780262b6
haobayuxi <whaohit@gmail.com>
2024-05-23 16:01:44 +0800

    update

--- compute_pool/worker/worker.h
+++ compute_pool/worker/worker.h
@@ -6,8 +6,9 @@
 #include "allocator/region_allocator.h"
 #include "base/common.h"
 #include "connection/meta_manager.h"
-#include "metadata/dmc.h"
+// #include "metadata/dmc.h"
 #include "micro/micro_db.h"
+#include "micro/micro_meta.h"
 
 struct thread_params {
   t_id_t thread_local_id;
