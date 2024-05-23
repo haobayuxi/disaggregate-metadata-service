3def44abcbb34fdca82ba58f8e2f0370ea0509a5
haobayuxi <whaohit@gmail.com>
2024-05-23 17:47:11 +0800

    update

--- core/base/common.h
+++ core/base/common.h
@@ -53,15 +53,3 @@ const uint64_t MEM_STORE_META_END = 0xE0FF0E0F;
 // Helpful for improving condition prediction hit rate
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #define likely(x) __builtin_expect(!!(x), 1)
-
-uint64_t compute_hash(string const& s) {
-  const int p = 31;
-  const int m = 1e9 + 9;
-  uint64_t hash_value = 0;
-  uint64_t p_pow = 1;
-  for (char c : s) {
-    hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
-    p_pow = (p_pow * p) % m;
-  }
-  return hash_value;
-}
--- core/metadata/dmc.h
+++ core/metadata/dmc.h
@@ -117,4 +117,16 @@ void DMC::AddToReadOnlySet(DataItemPtr item) {
 
 // uint64_t get_permission_offset(uint64_t addr) { return addr; }
 
+int64_t compute_hash(string const& s) {
+  const int p = 31;
+  const int m = 1e9 + 9;
+  uint64_t hash_value = 0;
+  uint64_t p_pow = 1;
+  for (char c : s) {
+    hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+    p_pow = (p_pow * p) % m;
+  }
+  return hash_value;
+}
+
 #endif
