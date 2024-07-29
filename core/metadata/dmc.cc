62b45bd7db114b9740fc51f1cdfef6f18ca499e7
haobayuxi <whaohit@gmail.com>
2024-07-29 15:31:14 +0800

    update

--- core/metadata/dmc.cc
+++ core/metadata/dmc.cc
@@ -19,43 +19,7 @@ DMC::DMC(MetaManager* meta_man, QPManager* qp_man, t_id_t tid, coro_id_t coroid,
   servers = servers_;
 }
 
-bool DMC::open(string path, coro_yield_t& yield) {
-  std::vector<DirectRead> pending_direct_ro;
-  std::vector<HashRead> pending_hash_ro;
-  vector<NameID> paths = path_resolution(path);
-  if (dmc_type == DMC_TYPE::native) {
-    // get all the inode
-  } else if (dmc_type == DMC_TYPE::disaggregated) {
-    // check cache
-    for (NameID id : paths) {
-      InodeCacheItem cache_inode;
-      int node_id = id.key % servers;
-      if (addr_cache->Search(node_id, id.key, InodeCacheItem & cache_inode)) {
-        if (cache_inode.inode.is_dir) {
-          // is dir check permission
-
-        } else {
-          // not dir, insert into readerset
-          DataItemPtr dir = std::make_shared<DataItem>(0, id.key);
-        }
-      } else {
-        // insert into readset
-        // reader_set.push_back();
-      }
-    }
-  }
-  if (!IssueReader(pending_direct_ro, pending_hash_ro)) return false;
-  // Yield to other coroutines when waiting for network replies
-  coro_sched->Yield(yield, coro_id);
-
-  // Receive data
-  std::list<HashRead> pending_next_hash_ro;
-  auto res = CheckReadRO(pending_direct_ro, pending_hash_ro,
-                         pending_next_hash_ro, yield);
-  //   check results
-
-  return true;
-}
+bool DMC::open(string path, coro_yield_t& yield) { return true; }
 bool DMC::close(string path, coro_yield_t& yield) { return true; }
 
 bool DMC::stat_file(string path, coro_yield_t& yield) {
@@ -68,8 +32,8 @@ bool DMC::stat_file(string path, coro_yield_t& yield) {
     // check cache
     for (NameID id : paths) {
       InodeCacheItem cache_inode;
-
-      if (addr_cache->Search(node_id, it->key, InodeCacheItem & cache_inode)) {
+      auto node_id = id.key % servers;
+      if (addr_cache->Search(node_id, id.key, InodeCacheItem & cache_inode)) {
         if (cache_inode.inode.is_dir) {
           // is dir check permission
 
@@ -98,37 +62,6 @@ bool DMC::stat_file(string path, coro_yield_t& yield) {
 bool DMC::read(string path, coro_yield_t& yield) { return true; }
 bool DMC::write(string path, coro_yield_t& yield) { return true; }
 bool DMC::create(string path, coro_yield_t& yield) {
-  std::vector<DirectRead> pending_direct_ro;
-  std::vector<HashRead> pending_hash_ro;
-  vector<NameID> paths = path_resolution(path);
-  if (dmc_type == DMC_TYPE::native) {
-    // get all the inode
-  } else if (dmc_type == DMC_TYPE::disaggregated) {
-    // check cache
-    for (NameID id : paths) {
-      InodeCacheItem cache_inode;
-
-      if (addr_cache->Search(node_id, it->key, InodeCacheItem & cache_inode)) {
-        if (cache_inode.inode.is_dir) {
-          // is dir check permission
-
-        } else {
-          // not dir, insert into writeset
-        }
-      } else {
-        // insert into readset
-        // reader_set.push();
-      }
-    }
-  }
-  if (!IssueReader(pending_direct_ro, pending_hash_ro)) return false;
-  // Yield to other coroutines when waiting for network replies
-  coro_sched->Yield(yield, coro_id);
-
-  // Receive data
-  std::list<HashRead> pending_next_hash_ro;
-  auto res = CheckReadRO(pending_direct_ro, pending_hash_ro,
-                         pending_next_hash_ro, yield);
   //   check results
 
   return true;
